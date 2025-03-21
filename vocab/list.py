5d8a4488faeaba1734f7d1a9c4412dfe922fd96e
Sam Zilli <samzilli01@gmail.com>
2025-03-21 10:23:46 -0400

    notes for more stuff to do

---
+++ vocab/list.py
@@ -0,0 +1,13 @@
+vocab_terms = {
+    "ProgrammingCode": "Programming code is a set of written computer instructions, guided by rules, using a computer programming language. It tells computers and machines how to perform an action.",
+    "ProgrammingLanguages": "Programming languages are used to write computer programs. They are similar to human spoken languages in that they both use syntax and semantics.",
+    "Syntax": "Syntax is a set of rules for how statements are constructed in both human and computer languages. It includes rules for the order of elements and special characters.",
+    "Semantics": "Semantics refers to the intended meaning or effect of statements in both human and computer languages. Logical errors are also referred to as semantic errors.",
+    "ComputerProgram": "A computer program is a step-by-step list of instructions that a computer follows to reach a goal.",
+    "Script": "Scripts are usually shorter and less complex than computer programs. They are used to automate specific tasks but can be complex if needed.",
+    "Automation": "Automation replaces repetitive manual steps with actions that happen automatically.",
+    "Output": "Output is the end result of a task performed by a function or computer program, such as a value, report, or database entry.",
+    "Input": "Input is information provided to a program by the end user, including text, voice, images, and biometrics.",
+    "Functions": "A function is a reusable block of code that performs a specific task.",
+    "Variables": "Variables are used to temporarily store changeable values in programming code."
+}
